c0861125421b8c268796b0c6e7c63ceb1b9cce03
Nitin Saraswat <nitinsaraswat36@gmail.com>
2026-08-08 02:33:06 +0530

    Time: 0 ms (100%), Space: 23.8 MB (51.49%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,26 +1,49 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-
-        int n = nums.size();
-        vector<int> ans;
-
-        sort(nums.begin(), nums.end());
-
-        for (int i = 0; i < n; i++) {
-
-            int count = 1;
-
-            while (i + 1 < n && nums[i] == nums[i +1]) {
-                count++;
-                i++;
+        int n=nums.size();
+        vector<int>ans;
+        int ct1=0;
+        int ct2=0;
+        int el= INT_MIN;
+        int el1=INT_MIN;
+        for (int i=0;i<n;i++){
+            if(ct1==0&&nums[i]!=el1){
+                ct1++;
+                el=nums[i];
+            }
+            else if(ct2==0&&nums[i]!=el){
+                ct2++;
+                el1=nums[i];
+            }
+            else if(nums[i]==el) {
+                ct1++;
+            }
+            else if(nums[i]==el1){
+                ct2++;
+            }
+            else{
+                ct1--,ct2--;
             }
 
-            if (count > n / 3) {
-                ans.push_back(nums[i]);
-            }
+
         }
-
-        return ans;
+     int count1=0;
+     int count2=0;
+     for(int i=0;i<n;i++)   {
+        if(nums[i]==el){
+            count1++;
+        }
+        if(nums[i]==el1){
+            count2++;
+        }
+     }
+     if(count1>n/3){
+        ans.push_back(el);
+     }
+     if(count2>n/3){
+        ans.push_back(el1);
+     }
+    return ans;
     }
 };
