2ad662d16e971c32559a3e917ca0a2f8b69ea9ec
Nitin Saraswat <nitinsaraswat36@gmail.com>
2026-08-22 17:05:14 +0530

    Time: 0 ms (100%), Space: 14.1 MB (59.26%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -21,6 +21,7 @@ public:
 
    
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
         vector<vector<int>>ans;
         vector<int>ds;
         int n =candidates.size();
